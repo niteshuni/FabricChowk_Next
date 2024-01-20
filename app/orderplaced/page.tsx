77001fd7900d1166cf0262a727439d65a474dae3
Nitesh Mishra <nitesh.mishra@sap.com>
2024-01-20 21:09:55 +0530

    production issues

---
+++ app/orderplaced/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OrderPlaced = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default OrderPlaced;
---
+++ app/orderrec/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OrderRec = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default OrderRec;
